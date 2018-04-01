e88e49775c335bf82739fd60e6ea46dd4493449b
miller <millerpoli9@gmail.com>
2018-04-01 15:20:17 -0500

    caesar encrypt/decrypt with menu

--- i_cesar_encrypt_decrypt.py
+++ i_cesar_encrypt_decrypt.py
@@ -2,7 +2,8 @@
 
 
 def cesar_encrypt(n,message):
-	abc_list = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z']  
+	abc_list = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z']
+	zabc = ['abcdefghijklmnñopqrstuvwxyz']
 	temp = '?'
 	msn_to_encrypt = ""
 	i = 0
